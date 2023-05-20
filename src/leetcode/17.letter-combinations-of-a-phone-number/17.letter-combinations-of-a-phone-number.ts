fe2117e5effaaf4489c3c9236c987ae739d6fbc1
zhangjiayan <597071583@qq.com>
2023-05-20 23:45:35 +0800

    feat: Add LeetCode's problem 17

--- src/leetcode/17.letter-combinations-of-a-phone-number/17.letter-combinations-of-a-phone-number.ts
+++ src/leetcode/17.letter-combinations-of-a-phone-number/17.letter-combinations-of-a-phone-number.ts
@@ -1,28 +1,28 @@
 const mp: Map<string, string> = new Map<string, string>([
-    ['2', 'abc'],
-    ['3', 'def'],
-    ['4', 'ghi'],
-    ['5', 'jkl'],
-    ['6', 'mno'],
-    ['7', 'pqrs'],
-    ['8', 'tuv'],
-    ['9', 'wxyz']
-]);
+  ['2', 'abc'],
+  ['3', 'def'],
+  ['4', 'ghi'],
+  ['5', 'jkl'],
+  ['6', 'mno'],
+  ['7', 'pqrs'],
+  ['8', 'tuv'],
+  ['9', 'wxyz'],
+])
 
-function backTrace(res: string[], path:string, digits: string) {
-    if(path.length === digits.length){
-        res.push(path);
-    }
-    let index = path.length;
-    for(const c of mp.get(digits[index]) || '') {
-        backTrace(res, path + c, digits);
-    }
+function backTrace(res: string[], path: string, digits: string) {
+  if (path.length === digits.length) {
+    res.push(path)
+  }
+  let index = path.length
+  for (const c of mp.get(digits[index]) || '') {
+    backTrace(res, path + c, digits)
+  }
 }
 
 export function letterCombinations(digits: string): string[] {
-    let res: string[] = [];
-    let path = "";
-    if(!digits) return res; 
-    backTrace(res, path, digits);
-    return res;
-};
+  let res: string[] = []
+  let path = ''
+  if (!digits) return res
+  backTrace(res, path, digits)
+  return res
+}
--- src/leetcode/17.letter-combinations-of-a-phone-number/__test__/17.letter-combinations-of-a-phone-number.test.ts
+++ src/leetcode/17.letter-combinations-of-a-phone-number/__test__/17.letter-combinations-of-a-phone-number.test.ts
@@ -2,12 +2,12 @@ import { letterCombinations } from '../17.letter-combinations-of-a-phone-number'
 
 describe('lengthOfLongestSubstring', () => {
   it('test case 1', () => {
-    expect(letterCombinations("2")).toEqual(["a","b","c"])
+    expect(letterCombinations('2')).toEqual(['a', 'b', 'c'])
   })
   it('test case 2', () => {
-    expect(letterCombinations("23")).toEqual(["ad","ae","af","bd","be","bf","cd","ce","cf"])
+    expect(letterCombinations('23')).toEqual(['ad', 'ae', 'af', 'bd', 'be', 'bf', 'cd', 'ce', 'cf'])
   })
   it('test case 3', () => {
-    expect(letterCombinations("")).toEqual([])
+    expect(letterCombinations('')).toEqual([])
   })
 })

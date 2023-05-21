94b9c4bc1f34bf0b350775100884166a5d894b93
lovezhangchuangxin <2911331070@qq.com>
2023-05-21 16:18:35 +0800

    feat: climb stairs

---
+++ src/leetcode/70.climbing-stairs/__test__/70.climbing-stairs.test.ts
@@ -0,0 +1,10 @@
+import { climbStairs } from '../70.climbing-stairs'
+
+describe('climbStairs', () => {
+  it('test case 1', () => {
+    expect(climbStairs(2)).toBe(2)
+  })
+  it('test case 2', () => {
+    expect(climbStairs(3)).toBe(3)
+  })
+})
